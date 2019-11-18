b0b179ff8e69f23d04f31a8fb7d0499dce46e6bb
sunilkumar35 <rkt.sunilkumar97@gmail.com>
2019-11-18 19:35:49 +0530

    person list project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => <PersonList/>;
+
+
+const PersonList  = () => {
+  
+  const people = [
+    {
+      img:21,name:"jhon",job:"developer",
+    },
+    {
+      img:28,name:"dexter",job:"designer",
+    },
+    {
+      img:37,name:"alen",job:"artist",
+    },
+  ]
+  
+  return (<section>
+    <Person person={people[0]}/>
+    <Person person={people[1]}>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's.
+    </Person>
+    <Person person={people[2]}/>
+  </section>)
+};
+
+const Person = (props) => {
+
+  const {img , name , job } = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='person'> 
+    <img src={url} alt=''/>
+    <div>
+      <h4>{name}</h4>
+      <h3>{job}</h3>
+      {children}
     </div>
-  );
+    
+    </div>
+  )
 }
+
 
 export default App;
